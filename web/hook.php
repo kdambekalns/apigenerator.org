95b2931c343fa60ad35bededd72acc11db9f4b29
Karsten Dambekalns <karsten@dambekalns.de>
2017-01-31 12:45:34 +0100

    Simplify webhook

--- web/hook.php
+++ web/hook.php
@@ -3,7 +3,6 @@
 /**
  * ApiGenerator.org
  * Copyright (C) 2013 Tristan Lins
- *
  * PHP version 5
  *
  * @copyright  bit3 UG 2013
@@ -13,4 +12,28 @@
  * @filesource
  */
 
-include(__DIR__ . '/github.php');
+ob_start();
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (!array_key_exists('payload', $_POST)) {
+    header("HTTP/1.1 400 Bad Request");
+    echo '400 Bad Request';
+    while (count(ob_list_handlers())) {
+        ob_end_flush();
+    }
+    exit(1);
+}
+
+$payload = $_POST['payload'];
+$payload = json_decode($payload);
+
+$repository = \ApiGeneratorOrg\Repository::createFromGithubPayload($payload);
+$source = new \ApiGeneratorOrg\GithubSource();
+
+$hook = new \ApiGeneratorOrg\Hook();
+$hook->run($repository, $source);
+
+while (count(ob_list_handlers())) {
+    ob_end_flush();
+}
